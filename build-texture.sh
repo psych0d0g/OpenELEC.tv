83a420d3db01924c2e9804c1145f68c299bbc6b3
Dale Hamel <dale.hamel@srvthe.net>
2013-11-26 13:39:22 -0500

    Think I've fixed ffmpeg installation

---
+++ build-texture.sh
@@ -0,0 +1,5 @@
+
+#cp ./rasplex-settings/resources/skins/Default/media/* ./plex-home-theater/addons/skin.mediastream/Media/
+
+#./TexturePacker -input ./plex-home-theater/addons/skin.mediastream/Media/ -output ./plex-home-theater/addons/skin.mediastream/Media/Textures.xbt
+./TexturePacker -input ./plex-home-theater/addons/skin.plex/Media/ -output ./plex-home-theater/addons/skin.plex/Media/Textures.xbt
